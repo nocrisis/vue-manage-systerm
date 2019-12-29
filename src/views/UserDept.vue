4e774fb1655f476c4e0f8c7f212093c2cb6f96de
15125296181@163.com <15125296181@163.com>
2019-12-29 22:38:42 +0800

    user list

--- src/views/UserDept.vue
+++ src/views/UserDept.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="fill_content">
-    <el-button
-      icon="el-icon-plus"
-      type="danger"
-      @click="handleAdd()"
-      circle
-    ></el-button>
     <div class="dept_tree">
+      <el-button
+        icon="el-icon-plus"
+        type="danger"
+        @click="handleDeptAdd()"
+        circle
+      ></el-button>
       <el-table
         :data="tableData"
         style="width: 100%;margin-bottom: 20px;"
         row-key="id"
         border
         default-expand-all
+        highlight-current-row
+        @current-change="handleCurrentDeptChange"
         :tree-props="{ children: 'deptList', hasChildren: 'hasChildren' }"
       >
         <el-table-column prop="id" label="ID" sortable width="70">
@@ -21,35 +23,93 @@
         </el-table-column>
         <el-table-column label="操作" width="150">
           <template slot-scope="scope">
-            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+            <el-button
+              size="mini"
+              @click="handleDeptEdit(scope.$index, scope.row)"
               >编辑</el-button
             >
             <el-button
               size="mini"
               type="danger"
-              @click="handleDelete(scope.$index, scope.row)"
+              @click="handleDeptDelete(scope.$index, scope.row)"
               >删除</el-button
             >
           </template>
         </el-table-column>
       </el-table>
     </div>
-    <div class="user_list"></div>
-    <Dialog
+    <div class="user_list">
+      <el-button
+        icon="el-icon-plus"
+        type="danger"
+        @click="handleUserAdd()"
+        circle
+      ></el-button>
+      <el-table
+        :data="userList"
+        style="width: 100%;margin-bottom: 20px;"
+        row-key="id"
+        border
+        default-expand-all
+      >
+        <el-table-column prop="id" label="ID" sortable width="70">
+        </el-table-column>
+        <el-table-column prop="username" label="用户名称" width="100">
+        </el-table-column>
+        <el-table-column prop="telephone" label="手机号" width="100">
+        </el-table-column>
+        <el-table-column prop="mail" label="邮箱" width="100">
+        </el-table-column>
+        <el-table-column prop="memo" label="备注" width="100">
+        </el-table-column>
+        <el-table-column label="状态" width="100">
+          <template slot-scope="scope">
+            {{ statusEnum[scope.row.status] }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="150">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleUserEdit(scope.$index, scope.row)"
+              >编辑</el-button
+            >
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDeptDelete(scope.$index, scope.row)"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <DeptDialog
       :dept_dialog="dept_dialog"
       :add_depart="add_depart"
       @updateData="getDeptTree"
       :option_list="tableData"
-    ></Dialog>
+    ></DeptDialog>
   </div>
 </template>
 <script>
-import Dialog from '../components/DeptDialog'
+import DeptDialog from '../components/DeptDialog'
 
 export default {
   data() {
     return {
+      statusEnum: {
+        1: '正常',
+        0: '冻结',
+        2: '删除'
+      },
+      queryParam: {
+        pageSize: 10,
+        pageNo: 1,
+        deptId: ''
+      },
       tableData: [],
+      userList: [],
       dept_dialog: {
         show: false,
         title: '',
@@ -85,7 +145,7 @@ export default {
         }
       })
     },
-    handleEdit(index, row) {
+    handleDeptEdit(index, row) {
       this.dept_dialog = { show: true, title: '修改部门信息', option: 'edit' }
       this.add_depart = {
         id: row.id,
@@ -95,7 +155,7 @@ export default {
         memo: row.memo
       }
     },
-    handleDelete(index, row) {
+    handleDeptDelete(index, row) {
       this.$axios.delete(`/api/sys/dept/delete/${row.id}`).then(res => {
         console.log('delete:', res)
         if (res.data.code == 200) {
@@ -104,7 +164,7 @@ export default {
         this.getDeptTree()
       })
     },
-    handleAdd() {
+    handleDeptAdd() {
       this.dept_dialog = { show: true, title: '添加部门信息', option: 'add' }
       this.add_depart = {
         id: '',
@@ -113,10 +173,33 @@ export default {
         seq: '',
         memo: ''
       }
+    },
+    handleCurrentDeptChange(curRow) {
+      this.queryParam.deptId = curRow.id
+      this.$axios.post('/api/sys/user/list', this.queryParam).then(res => {
+        console.log('listUser', res)
+        let result = res.data
+        if (result.code == 200) {
+          let data = result.data
+          if (data.total > 0) {
+            this.userList = data.data
+          }
+        } else {
+          this.$message(result.msg)
+        }
+      })
     }
   },
   components: {
-    Dialog
+    DeptDialog
   }
 }
 </script>
+<style scoped>
+.dept_tree {
+  margin-right: 10%;
+}
+.user-list {
+  flex-grow: 1;
+}
+</style>
